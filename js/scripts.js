517053a75094b79827110e1d0a5c38e5e970e343
Daniela Serna <daniela@s1portland.com>
2019-02-24 18:42:56 -0800

    "Add divs and build jQuery."

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,24 @@
 $(document).ready(function(){
-  var age = parseInt(prompt("How old are you?"));
+  $("form#submitSurvey").submit(function(event) {
+  event.preventDefault();
+  var beverage = $("#beverage").parseInt(val());
+  var treat = $("#treat").val();
+  var time = $("#time").(val();
+  var day = $("#day").(val();
+  var school = $("#school").val();
 
-  if (age >= 18) {
-    $('#choices').show();
-  } else {
-    $('#under-18').show();
+  var userScore = addScore(beverageScore, treatScore, timeScore, dayScore, schoolScore);
+
+  if (userScore <10= ) {
+    (destination = "Lisbon, Portugal!");
   }
+  else if (userScore >=16) {
+    (destination = "Tokyo, Japan!");
+  }
+  else {
+    (destination = "Mexico City, Mexico!");
+  }
+
+  $("#output").text(result);
+
 })
